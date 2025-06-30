21ffe03c8f517fbe30b9de53b0da26803c4c24eb
SzilviStnr <steinerszilvi@gmail.com>
2025-06-30 04:03:41 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import LioraApp from './components/LioraApp';
 import { UserProvider } from './UserContext';
 import PresenceIndicator from './components/PresenceIndicator';
 
 function App() {
- return (
-  <UserProvider>
-    <LioraApp />
-    <PresenceIndicator />
-  </UserProvider>
-);
-}
+  // Példa egy állapot változóra a mélység értékére
+  const [depth, setDepth] = useState(45);
 
+  useEffect(() => {
+    // Itt később lehet valós adatot lekérni és beállítani, most maradhat így
+  }, []);
+
+  return (
+    <UserProvider>
+      <LioraApp />
+      <PresenceIndicator depth={depth} />
+    </UserProvider>
+  );
+}
 
 export default App;
