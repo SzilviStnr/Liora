013715e9db753fa08d5cbee984547684e90d9698
SzilviStnr <steinerszilvi@gmail.com>
2025-06-30 03:35:05 +0200

    Updated PresenceIndicator.tsx

---
+++ src/components/PresenceIndicator.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../index.css'; // vagy ahol a CSS van
+
+export default function PresenceIndicator() {
+  return (
+    <div id="presence-indicator">
+      <p>Jelenlét: Aktív</p>
+    </div>
+  );
+}
