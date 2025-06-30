2cf0cc9412f7d19b9afaad17dea4c5c68fa632d9
SzilviStnr <steinerszilvi@gmail.com>
2025-07-01 00:58:58 +0200

    Fix main.tsx component import

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import LioraApp from './components/LioraApp.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <LioraApp />
   </StrictMode>
 );
